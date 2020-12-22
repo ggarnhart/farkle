159b5d23e4d356ea94777231fb973058e8870a32
Greg Garnhart <greg.garnhart@sap.com>
2020-12-22 11:21:26 -0500

    fixed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js, jsx, ts, tsx}",
-    "./components/**/*.{js, jsx, ts, tsx}",
-  ],
+  purge: ["./pages/*.{js, jsx, ts, tsx}", "./components/*.{js, jsx, ts, tsx}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
